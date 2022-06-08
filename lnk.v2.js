1342a1d597e0c75dbd98b8d0546a34f65e123626
jquery20 <96495928+jquery20@users.noreply.github.com>
2022-06-09 00:57:24 +0530

    Create lnk.v2.js

---
+++ lnk.v2.js
@@ -0,0 +1,17 @@
+<script>
+function getURLParameter(name) {
+	return decodeURI((RegExp(name + '=' + '(.+?)(&|$)').exec(location.search)||[,null])[1]);
+}
+function hideURLParams() {
+	//Parameters to hide (ie ?success=value, ?error=value, etc)
+	var hide = ['link'];
+	for(var h in hide) {
+		if(getURLParameter(h)) {
+			history.replaceState(null, document.getElementsByTagName("title")[0].innerHTML, window.location.pathname);
+		}
+	}
+}
+
+//Run onload, you can do this yourself if you want to do it a different way
+window.onload = hideURLParams;
+</script>
